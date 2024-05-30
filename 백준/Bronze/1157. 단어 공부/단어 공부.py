f1900b5188d8cb30315fef6e4b0818f5b5ded564
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-05-30 13:25:47 +0900

    [Bronze I] Title: 단어 공부, Time: 80 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,28 +1,19 @@
-from collections import Counter
-
 #대문자로 치환 후 문자별 카운트
-#value가 같다면 -> ?출력
+#최빈값이 유일하지 않다면 -> ?출력
 #빈도수 가장 큰 문자 대문자로 출력
 
 word=input().upper()
-word_counter=Counter(word)
+word_list = list(set(word)) #단어 사전 리스트
+count_list=[]
 
-#빈도수가 가장 큰 value를 찾아서 1도 같은지판단
 
-#오름차순으로 정렬
-value_list = sorted(word_counter.items(), key=lambda x:x[1],reverse=True)
+for i in word_list:
+    count=word.count(i) #word에 해당 문자열이 몇개있는지
+    count_list.append(count)
+
 
 #max값이 유일한지 확인
-if len(value_list)==1:
-    print(value_list[0][0])
-
+if count_list.count(max(count_list))>1:
+    print('?')
 else:
-    if value_list[0][1]==value_list[1][1]:
-        print('?')
-    else:
-        print(value_list[0][0])
-
-
-
-
-
+    print(word_list[count_list.index(max(count_list))])
