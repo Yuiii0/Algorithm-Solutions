963c1d56b9341871005ce864df4bb07352347e53
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-05-31 19:05:38 +0900

    [Silver IV] Title: 듣보잡, Time: 96 ms, Memory: 43920 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,30 +1,20 @@
-# 1. 듣지 못한 사람, 보지 못한 사람 리스트 따로 만들어서 교집합으로 구하기
-# 2. 공통의 dict만들어서 value가 2인사람=듣보잡
+import sys
+input = sys.stdin.readline
 
-n,m=input().split()
+n, m = input().rstrip().split()
 
+# 명단 dict 생성
+users = {}
 
-#명단 dict 생성
-users={}
+for _ in range(int(n) + int(m)):
+    name = input().rstrip()
+    users[name] = users.get(name, 0) + 1
 
-for i in range(int(n)+int(m)):
-    name=input()
-    users[name]=users.get(name,0)+1
-
-# dict_items([('ohhenrie', 2), ('charlie', 1), ('baesangwook', 2), ('obama', 1), ('clinton', 1)])
-
-#value가 2인 듣보잡 찾기
-result=list(filter(lambda x:x[1]==2, users.items()))
+# value가 2인 듣보잡 찾기
+result = [name for name, count in users.items() if count == 2]
 result.sort()
 
-#출력
+# 출력
 print(len(result))
-for r in result:
-    print(r[0]) #듣보잡 출력
-
-
-
-
-    
-
-
+for name in result:
+    print(name)
