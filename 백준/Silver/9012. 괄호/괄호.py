021f6dfebe622868e80a46d213b1ec404da0c0e0
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-05 16:53:20 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -5,22 +5,23 @@ n = int(input())
 
 for i in range(n):
     stack = []
-    temp = list(input().strip())  # .strip()을 사용하여 입력의 양 끝 공백을 제거
-    valid = True  # 유효성을 체크하는 변수 추가
+    temp = list(input().strip())  
+    valid = True  
 
     for bracket in temp:
-        # 여는 괄호는 저장
+        # open 괄호는 저장
         if bracket == '(':
             stack.append(bracket)
+        # close 괄호라면 stack에서 짝을 이루는 괄호 꺼내기
         else:
             if stack:
                 stack.pop()
             else:
                 print('NO')
-                valid = False  # 잘못된 경우 valid를 False로 설정
+                valid = False  
                 break
 
-    if valid:  # 루프가 정상적으로 종료된 경우에만 stack을 체크
+    if valid:  
         if stack:
             print('NO')
         else:
