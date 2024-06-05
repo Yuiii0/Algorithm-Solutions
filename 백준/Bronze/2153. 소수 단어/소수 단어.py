3f94a6b6a4f29c17619e4aed7bbb18a6bc699a2e
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-05 13:23:37 +0900

    [Bronze II] Title: 소수 단어, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2153. 소수 단어/소수 단어.py
+++ 백준/Bronze/2153. 소수 단어/소수 단어.py
@@ -5,19 +5,7 @@ import sys
 import math
 input=sys.stdin.readline
 
-words = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
-    'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
-    'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
-    'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
-    'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
-    'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30,
-    'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35,
-    'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40,
-    'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45,
-    'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50,
-    'Y': 51, 'Z': 52
-}
+words=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 input_word=input().strip()
 
@@ -32,11 +20,8 @@ def is_prime(n):
 
 
 #숫자로 변환
-num_word=0
-num_word=sum(words.get(s,0) for s in input_word)
-# for s in input_word:
-#     num_word+=
-#print(num_word)
+num_word=sum(words.index(s)+1 for s in input_word)
+
 
 
 #소수단어 판별
