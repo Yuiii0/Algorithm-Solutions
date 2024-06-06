f270f14fa6a311f969e8e101bbc701ce61ef13b6
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-06 14:05:56 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,16 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n=int(input())
-
+r=31
+M=1234567891
+#알파벳:정수 hash 생성
 input_hash={chr(ord('a')+i) :i+1 for i in range(26)}            
 
 s=input()
 result=0
 
 for i in range(n):
+    # 1.정수 치환
     str_to_num=input_hash[s[i]]
-    result+=str_to_num*(31**i)
-print(result)
+    # 2. sum
+    result+=str_to_num*(r**i)
+
+# mod
+print(result%M)
     
  
         
