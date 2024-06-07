0c7657381193a14f18d0d8eef126f2a7e98c116d
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-07 15:38:10 +0900

    [Silver II] Title: 나무 자르기, Time: 2228 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,27 +1,35 @@
 import sys
-input = sys.stdin.readline
-n,target=map(int,input().split())
-trees=list(map(int,input().strip().split()))
 
-left=1
-right=max(trees)
-result=0
+def find_height(trees,target):
+    left = 1
+    right = max(trees)
+    result = 0
 
-while left<=right:
-    mid=(left+right)//2
-    count=0
-    # 얻을 수 있는 나무 개수 
-    for tree in trees:
-        if tree>mid:
-            count+=tree-mid
+    while left <= right:
+        mid = (left + right) // 2
+        count = 0
+        # 얻을 수 있는 나무 개수
+        for tree in trees:
+            if tree > mid:
+                count += tree - mid
 
-    # mid 조정
-    if count>=target:
-        result=mid
-        left=mid+1
-    else:
-        right=mid-1
-print(result)
-    
+        # mid 조정
+        if count >= target:
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return result
 
 
+
+def main():
+    input = sys.stdin.readline
+    n, target = map(int, input().split())
+    trees = list(map(int, input().strip().split()))
+    answer=find_height(trees,target)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
