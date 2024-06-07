49dd18481b74eb537a9711d0bab17d04e64c7ac1
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-07 14:02:04 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -6,6 +6,5 @@ N=int(input())
 univ_list=[] # [university,num]
 for _ in range(N):    
     M=int(input())
-    for _ in range(M):
-        univ_list.append(input().split())
+    univ_list=[input().split() for _ in range(M)]
     print(sorted(univ_list,key=lambda x:-int(x[1]))[0][0])
