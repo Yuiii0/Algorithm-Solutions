8284fd99fc3f4eb031a3d966c7631b4a291224ac
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-07 16:08:56 +0900

    [Bronze II] Title: 팀명 정하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
@@ -1,27 +1,31 @@
 import sys
-input = sys.stdin.readline
 
-year_list=[]
-sol_name=[]
-# 첫번재 방법
-for _ in range(3):
+def first_naming(years):
+    years.sort()
+    result = ''.join(map(str, years))
+    return result
+
+def sec_naming(sol_name):
+    sol_name.sort(key=lambda x: -int(x[0]))
+    result = ''.join([t[1][0] for t in sol_name])
+    return result
+
+def read_input():
+    input = sys.stdin.readline
+    year_list = []
+    sol_name = []
+    for _ in range(3):
         sol, year, name = input().rstrip().split()
-        year_list.append(int(year)%100)
-        sol_name.append((sol,name))
-year_list.sort()
-print(''.join(map(str,year_list)))
+        year_list.append(int(year) % 100)
+        sol_name.append((sol, name))
+    return year_list, sol_name
 
+def main():
+    year_list, sol_name = read_input()
+    res1 = first_naming(year_list)
+    res2 = sec_naming(sol_name)
+    print(res1)
+    print(res2)
 
-
-# 두번째 방법
-sol_name.sort(key=lambda x:-int(x[0]))
-for t in sol_name:
-    print(t[1][0],end='')
-
-
-
-        
-        
-
-
-    
+if __name__ == "__main__":
+    main()
