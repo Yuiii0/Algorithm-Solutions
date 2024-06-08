8e31a21b006650c29b3c5da38a3b1ca5e77b4074
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-08 16:50:43 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,29 +1,25 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-# 입력 받기
-n, m = map(int, input().strip().split())
+n,m=map(int,input().strip().split())
+z = m * 100 // n # 현재 승률 
 
-# 현재 승률 계산
-z = m * 100 // n
 
-# 승률이 100%이면 더 이상 변화하지 않음
 if z >= 99:
     print(-1)
 else:
-    # 이분 탐색 초기 범위 설정
-    start = 1
-    end = 1000000000  # 충분히 큰 숫자로 설정
-
-    # 이분 탐색
-    result = -1
-    while start <= end:
-        mid = (start + end) // 2
+    
+    
+    start=1
+    end=n
+    
+    result=0
+    while start<=end:
+        mid=(start+end)//2
         new_z = (m + mid) * 100 // (n + mid)
-        if new_z > z:
-            result = mid
-            end = mid - 1
+        if z+1<=new_z: #오른쪽 탐색
+            result=mid
+            end=mid-1
         else:
-            start = mid + 1
-
-    print(result)
+            start=mid+1
+    print(result)    
