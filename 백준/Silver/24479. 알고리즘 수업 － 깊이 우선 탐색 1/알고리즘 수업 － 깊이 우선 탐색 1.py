8ef2b42d5d31c75f1bb365ef972ea87763b6329f
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-10 14:18:57 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 744 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+cnt=1
+def dfs(idx):
+    global cnt
+    if visited[idx]:
+        return
+    visited[idx] = cnt  # 방문한 순서
+
+    # 인접리스트 순회하면서 dfs 재귀호출
+    for i in graph[idx]:
+        if visited[i]==0:  # 방문안한 노드
+            cnt+=1
+            dfs(i)
+
+
+n, m, r = map(int, input().rstrip().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+
+for _ in range(m):
+    a, b = map(int, input().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 오름차순 순서로 인접노드를 방문하기 위해 정렬
+for g in graph:
+    g.sort()
+
+dfs(r)
+
+
+# 출력
+for i in range(1, n + 1):
+    print(visited[i])
+
+
+
+
