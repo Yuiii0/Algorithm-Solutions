742cadb162d6e08971ba2de6fa06d740f3d8261f
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-11 11:08:34 +0900

    [Silver I] Title: 숨바꼭질, Time: 252 ms, Memory: 40316 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,35 +2,36 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(start, target):
-    queue = deque([start])
-    visited[start] = 0  # 시작위치=0
+
+# 최단경로 찾기=최단시간 찾기 (bfs)
+
+def bfs(start,target):
+    global cur
+    queue=deque([start])
+    visited[start]=0 #시작점은 이동거리=0
 
     while queue:
-        cur = queue.popleft()
+        cur=queue.popleft()
 
-        if cur == target:
-            return visited[cur]  # 이동 시간(s)
+        if cur==target:
+            return visited[cur]
 
         for i in range(3):
-            if i < 2:  # 앞으로 또는 뒤로 이동
-                nx = cur + dx[i]
-            else:  # 텔레포트 이동
-                nx = cur * dx[i]
-
-            # 경계 체크 및 방문 여부 확인
+            if i<2:
+                nx=cur+dx[i]
+            else:
+                nx=cur*dx[i]
+            
             if 0 <= nx <= max(n, m) * 2 and visited[nx] == -1:
-                visited[nx] = visited[cur] + 1
+                visited[nx]=visited[cur]+1
                 queue.append(nx)
 
-n, m = map(int, input().rstrip().split())
-visited = [-1] * (max(n, m) * 2 + 1)  
-cur = n  # 수빈 현재 위치
-dx = [1, -1, 2]  # 수빈의 이동 방식
 
-# 시작 위치가 목표 위치와 같은 경우
-if n == m:
-    print(0)
-else:
-    result = bfs(cur, m)
-    print(result)
+
+n, m = map(int, input().rstrip().split())
+visited = [-1] * (max(n, m) * 2 + 1) #두배까지 이동할수 있기 때문
+cur = n  # 수빈 현재위치
+dx = [1, -1, 2]  # 2x=cur+cur
+
+result=bfs(cur, m)
+print(result)
