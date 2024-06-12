983baea389cdbf751ced8e532eff5aa4feff1797
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-12 12:15:12 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import combinations
+
+# 가능한 3가지 카드의 모든 조합 생성 (combinations)
+# 가장 m과 가까운값 찾기
+
+input=sys.stdin.readline
+n,m=map(int,input().rstrip().split())
+num_list=list(map(int,input().rstrip().split()))
+
+result=0
+for comb in combinations(num_list,3):
+    # m보다 값이 작거나 같고, result에 저장해둔 현재 최대값보다 크다면, 업데이트
+    if sum(comb)<=m and result<sum(comb):
+        result=sum(comb) 
+
+print(result)
+    
+
