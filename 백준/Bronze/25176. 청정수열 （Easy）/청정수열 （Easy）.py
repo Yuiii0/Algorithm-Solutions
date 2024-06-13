c5ce8c505b410efe680202e7b5a8d0723c066ebb
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-13 19:41:54 +0900

    [Bronze I] Title: 청정수열 (Easy), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25176. 청정수열 （Easy）/청정수열 （Easy）.py
@@ -0,0 +1,14 @@
+                                 
+import sys                       
+input=sys.stdin.readline         
+                                 
+n=int(input())                   
+                                 
+def factorial(n):                
+    if n==1:                     
+        return 1                 
+    return n*factorial(n-1)      
+                                 
+result=factorial(n)              
+print(result)                    
+                                 
