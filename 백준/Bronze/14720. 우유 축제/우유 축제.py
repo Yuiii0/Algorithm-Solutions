938f55b88e2ee0c3e51da56dafd18d34ea6b218d
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-14 13:40:44 +0900

    [Bronze II] Title: 우유 축제, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.py
+++ 백준/Bronze/14720. 우유 축제/우유 축제.py
@@ -2,53 +2,20 @@ import sys
 input=sys.stdin.readline
 
 # milks, stores 두개의 포인터 사용
+# stores를 순회하면서, milks의 포인터가 가리키는 숫자와 일치하는지 확인후, 카운트
+
 # 두 값이 일치하면, 둘 다 이동
 # 일치하지 않다면, store 포인터만 이동
 
 n=int(input())
 stores=list(map(int,input().rstrip().split()))
-milks=[0,1,2]*500
+milks=[0,1,2]
 count=0
 
 i=0
 for s in range(len(stores)):
     # print('포인터 위치',s,i,stores[s],milks[i])
-    if stores[s]==milks[i]:
+    if stores[s]==milks[i%len(milks)]:
         i+=1 #milk 포인터도 이동
         count+=1
 print(count)
-
-
-
-    # for j in range(milks):
-    #     print('포인터 위치', i, j, stores[i], milks[j])
-    #     if stores[i] == milks[j]:
-    #         i += 1  # 두 포인터 이동
-    #     else:  # 일치하지 않다면, stores만 이동
-    #         i += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
