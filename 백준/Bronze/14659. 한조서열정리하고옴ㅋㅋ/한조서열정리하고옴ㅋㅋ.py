ef5bef50981cc0827234ed5a6f79276c71e21b9b
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-14 12:06:30 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 48 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -1,22 +1,32 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n = int(input())
-peaks = list(map(int, input().rstrip().split()))
+n=int(input())
+peaks=list(map(int,input().rstrip().split()))
 
-best_man = peaks[0]
-count_list = []
-count = 0
-
-for i in range(1, n):
-    if best_man > peaks[i]:
-        count += 1
+best_man=peaks[0]
+count_list=[]
+count=0
+for i in range(1,len(peaks)):
+    if best_man>peaks[i]: # 처치 가능
+        count+=1
     else:
         count_list.append(count)
-        count = 0
-        best_man = peaks[i]
-
-# 마지막 count도 추가해야 합니다.
+        count=0 # 초기화
+        best_man=peaks[i]
 count_list.append(count)
-
 print(max(count_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
