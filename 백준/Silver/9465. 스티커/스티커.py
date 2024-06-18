3195238d018901ba2fb199acca86106a87174ee6
Yujiii0 <124478784+Yuiii0@users.noreply.github.com>
2024-06-18 12:26:44 +0900

    [Silver I] Title: 스티커, Time: 744 ms, Memory: 50064 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,32 +1,34 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(2)]
+tc=int(input())
 
-    # 2행 DP배열 형성
-    DP = [[0] * N for _ in range(2)]
+for _ in range(tc):
+    n=int(input())
+    stickers = [list(map(int, input().rstrip().split())) for _ in range(2)]
 
-    # 스티커 길이가 1일 경우
-    DP[0][0] = arr[0][0]
-    DP[1][0] = arr[1][0]
-    if N == 1:
-        print(max(DP[0][0], DP[1][0]))
+    dp=[[0]*n for _ in range(2)]
+
+    # 길이 1
+    dp[0][0]=stickers[0][0]
+    dp[1][0]=stickers[1][0]
+
+    if n==1:
+        print(max(dp[0][0], dp[1][0]))
         continue
 
-    # 스티커 길이가 2일 경우
-    DP[0][1] = arr[1][0] + arr[0][1]
-    DP[1][1] = arr[0][0] + arr[1][1]
-    if N == 2:
-        print(max(DP[0][1], DP[1][1]))
+    # 길이 2
+    dp[0][1]=stickers[1][0]+stickers[0][1]
+    dp[1][1]=stickers[0][0]+stickers[1][1]
+
+    if n==2:
+        print(max(dp[0][1],dp[1][1]))
         continue
 
-    # 스티커 길이가 3이상일 경우
-    for i in range(2, N):
-        # 메인 아이디어
-        DP[0][i] = max(DP[1][i - 2], DP[1][i - 1]) + arr[0][i]
-        DP[1][i] = max(DP[0][i - 2], DP[0][i - 1]) + arr[1][i]
+    # 길이 3
+    for i in range(2,n):
+        dp[0][i]=max(dp[1][i-2],dp[1][i-1])+stickers[0][i]
+        dp[1][i]=max(dp[0][i-2],dp[0][i-1])+stickers[1][i]
 
-    print(max(DP[0][-1], DP[1][-1]))
+    print(max(dp[0][-1],dp[1][-1]))
+
