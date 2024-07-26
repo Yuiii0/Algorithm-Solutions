d3e469c075105d8a93051e35eb30226db3a7db09
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-07-26 19:57:31 +0900

    [level 2] Title: 전화번호 목록, Time: 484.47 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,3 +1,13 @@
 def solution(phone_book):
-    answer = True
-    return answer
+    phone_map = {}
+
+    for number in phone_book:
+        phone_map[number]=True
+
+    for number in phone_book:
+        for i in range(1,len(number)):
+            prefix=number[:i] # 가능한 모든 후보 접두어 생성
+            if prefix in phone_map:
+                return False
+
+    return True
