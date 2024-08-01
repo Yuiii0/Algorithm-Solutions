24d8d7dd486fba5f0967b8fd748b5159ac15937d
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-08-01 23:02:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,14 +1,16 @@
+# 가장 맨앞 요소가 빠져나가야 다음 요소가 빠져나갈 수 있으므로 -> queue 사용
+from collections import deque
 def solution(cards1, cards2, goal):
+
     answer = ''
-    
-    for card in goal:
-        if len(cards1)>0 and card == cards1[0]: #cards가 비어있을때 오류방지
-            cards1.pop(0)
-        elif len(cards2)>0 and  card == cards2[0]:
-            cards2.pop(0)
-        
-        else :
-            return "No"
-            
-    
-    return "Yes"
+    first_cards=deque(cards1)
+    sec_cards=deque(cards2)
+
+    for word in goal:
+        if first_cards and word==first_cards[0]:
+            print(first_cards.popleft())
+        elif sec_cards and word==sec_cards[0]:
+            print(sec_cards.popleft())
+        else:
+            return 'No'
+    return 'Yes'
