246d6d9d15a8b8c42baa90862be3a592a9e49391
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-08-06 20:33:06 +0900

    [level 2] Title: 모음 사전, Time: 1.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,21 +1,13 @@
-def solution(word):
-    # 알파벳 모음 리스트
-    vowels = ['A', 'E', 'I', 'O', 'U']
-    
-    # 각 자리수에 대한 가중치 계산
-    weight = [781, 156, 31, 6, 1]  # 5^4 + 5^3 + 5^2 + 5^1 + 1
-    
-    # 결과 값 초기화
-    result = 0
-    
-    # 각 자리수에 따른 단어 위치 계산
-    for i, char in enumerate(word):
-        result += weight[i] * vowels.index(char) + 1
-    
-    return result
+from itertools import  product
 
-# 테스트 예시
-print(solution("AAAAE"))  # 6
-print(solution("AAAE"))   # 10
-print(solution("I"))      # 1563
-print(solution("EIO"))    # 1189
+
+def solution(word):
+    word_list=[]
+    vowel = ['A', 'E', 'I', 'O', 'U']
+
+    for i in range(1,6):
+        for per in product(vowel, repeat=i):
+            word_list.append(''.join(per))
+
+    word_list.sort()
+    return word_list.index(word)+1
