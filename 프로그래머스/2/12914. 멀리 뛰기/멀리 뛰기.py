255b9483c140ac6ae87f601058cf8a1ab4cb8569
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-08-13 14:13:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,26 +1,7 @@
-def factorial(x):
-    if x == 0 or x == 1:
-        return 1
-    result = 1
-    for i in range(2, x + 1):
-        result *= i
-    return result
-
-def combination(n, k):
-    if k > n or k < 0:
-        return 0
-    return factorial(n) // (factorial(k) * factorial(n - k))
-
 def solution(n):
-    MOD = 1234567
-    count = 0
+    answer=0
+    dp1, dp2=1,1
+    for i in range(2,n+1):
+        dp1,dp2=dp1+dp2,dp1
 
-    # 2칸의 개수를 i로 두고, 최대 2칸의 개수는 n//2
-    for i in range(n // 2 + 1):
-        # 1칸의 개수는 n - 2 * i
-        num_ones = n - 2 * i
-        # 전체의 경우의 수 계산
-        count += combination(num_ones + i, i)  # (num_ones + i)C(i)
-
-    return count % MOD
-
+    return dp1%1234567
