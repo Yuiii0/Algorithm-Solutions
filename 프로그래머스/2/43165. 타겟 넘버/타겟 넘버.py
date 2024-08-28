181629d51303b4b60fb7503146599bc2e105ffef
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-08-28 11:08:48 +0900

    [level 2] Title: 타겟 넘버, Time: 338.48 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,25 @@
+
+answer=0
+
+def dfs(numbers,target,cur_idx,cur_total):
+    global  answer
+
+    # 탈출 조건
+    if cur_idx==len(numbers):
+        if cur_total==target:
+            answer+=1
+        return
+
+    else:
+        dfs(numbers,target,cur_idx+1,cur_total+numbers[cur_idx])
+        dfs(numbers,target,cur_idx+1,cur_total-numbers[cur_idx])
+
+
+def solution(numbers, target):
+    global answer
+    answer=0
+
+    dfs(numbers,target,0,0)
+
+    return answer
+
