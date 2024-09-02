8bd7910b95f5c0a2e682e0c5657ceea42a10480e
yu_zi <124478784+Yuiii0@users.noreply.github.com>
2024-09-02 13:26:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.27 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 3.94 ms
+메모리: 11.7 MB, 시간: 4.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 12:27:18
+2024년 09월 02일 13:26:25
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,18 +1,11 @@
 def solution(s):
-    count=0
-    binary_num=s
+    turn=0
     zero_count=0
     
-    while binary_num!='1':
-        c=len(''.join(binary_num.split("0")))
-        temp=len(binary_num)-c
-        zero_count+=temp
-        binary_num=bin(c)[2:] 
-        count+=1
-        
-    
-    print(binary_num)
-    
-    
-    answer = []
-    return [count,zero_count]
+    while s!='1':
+        c=len(''.join(s.split("0")))
+        turn+=1
+        zero_count+=len(s)-c
+        s=bin(c)[2:] 
+   
+    return [turn,zero_count]
